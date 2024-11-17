c2b41527a68c9e770a1c87f3ab73c6d5c2cfca11
yael <howitzer117@outlook.com>
2024-11-17 10:38:51 -0600

    cambios xs

--- views/js/carrito.js
+++ views/js/carrito.js
@@ -116,7 +116,7 @@ async function comprarCarrito() {
 
     try {
         // Aquí incluimos el id_user junto con los productos en el cuerpo de la solicitud
-        const response = await fetch(`https://api-bikelike-vf/cart/${userId}/purchase`, {
+        const response = await fetch(`https://api-bikelike-vf.onrender.com/cart/${userId}/purchase`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
